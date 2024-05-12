c02236ba4852e9246ad2d1286e36eb5c254ce930
Rüçhan Adıgüzel <jceadsa@gmail.com>
2024-05-13 01:38:33 +0300

    projection bug fixed

--- Core/src/Core/Core/OrthographicCamera.cpp
+++ Core/src/Core/Core/OrthographicCamera.cpp
@@ -5,14 +5,15 @@ namespace FooGame
 
     void OrthographicCamera::RecalculateViewMatrix()
     {
-        m_Projection        = glm::ortho(m_Left, m_Rotation, m_Bottom, m_Top);
-        m_Projection[1][1] *= -1.0f;
+        constexpr int frustum  = 1000;
+        float aspect           = 16.0f / 9.0f;
+        m_Projection           = glm::ortho(m_Left, m_Right, m_Bottom, m_Top);
+        m_Projection[1][1]    *= -1.0f;
         glm::mat4 transform =
             glm::translate(glm::mat4(1.0f), m_Position) *
             glm::rotate(glm::mat4(1.0f), glm::radians(m_Rotation),
                         glm::vec3(0.0f, 0.0f, 1.0f));
-        m_View = transform;
-        // m_View[1][1] *= -1.0f;
+        m_View     = transform;
         m_ViewProj = m_Projection * m_View;
     }
 
