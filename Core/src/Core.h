e79c3506fa80c4815613cf011ffbe8d4f71233c3
Rüçhan Adıgüzel <jceadsa@gmail.com>
2024-05-21 23:09:53 +0300

    core library refactoring

---
+++ Core/src/Core.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Core/Core/Base.h"
+#include "Core/Core/UUID.h"
+
+#include "Core/File/FileHelper.h"
+
+#include "Core/Resources/LoadModel.h"
+
+#include "Core/Scene/Component.h"
+#include "Core/Scene/Entity.h"
+#include "Core/Scene/Scene.h"
+#include "Core/Scene/ScriptableEntity.h"
