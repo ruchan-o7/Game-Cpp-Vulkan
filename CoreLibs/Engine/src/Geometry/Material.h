366efb607ad9facbc1135a4689baa8bdfb4f04ea
Rüçhan Adıgüzel <44341663+ruchan-o7@users.noreply.github.com>
2024-05-30 16:43:22 +0300

    mesh processing refactored

--- CoreLibs/Engine/src/Geometry/Material.h
+++ CoreLibs/Engine/src/Geometry/Material.h
@@ -1,13 +1,12 @@
 #pragma once
 #include <cstdint>
 #include <string>
-namespace FooGame
-{
-    struct Material
-    {
-            uint32_t BaseColorTextureIndex  = 0;
-            uint32_t MetallicRoughnessIndex = 0;
-            uint32_t NormalTextureIndex     = 0;
-            std::string Name;
-    };
+namespace FooGame {
+  struct Material {
+	  int32_t     BaseColorTextureIndex  = 0;
+	  int32_t     MetallicRoughnessIndex = 0;
+	  int32_t     NormalTextureIndex     = 0;
+	  std::string Name;
+  };
 }  // namespace FooGame
+
--- Editor/src/EditorSceneDeserializer.cpp
+++ Editor/src/EditorSceneDeserializer.cpp
@@ -5,13 +5,14 @@
 #include <memory>
 #include <Core.h>
 #include <nlohmann/json.hpp>
+#include "nlohmann/json_fwd.hpp"
 #include <Log.h>
 #include <Engine.h>
 #include <tiny_obj_loader.h>
 namespace FooGame {
 
-  static void ApplyTransformation(MeshData&               meshData,
-								  nlohmann::basic_json<>& staticMesh) {
+  using json = nlohmann::json;
+  static void ApplyTransformation(MeshData& meshData, json& staticMesh) {
 	meshData.Transform.Translation.x = staticMesh["transform"]["position"]["x"];
 	meshData.Transform.Translation.y = staticMesh["transform"]["position"]["y"];
 	meshData.Transform.Translation.z = staticMesh["transform"]["position"]["z"];
@@ -24,11 +25,55 @@ namespace FooGame {
 	meshData.Transform.Scale.y = staticMesh["transform"]["scale"]["y"];
 	meshData.Transform.Scale.z = staticMesh["transform"]["scale"]["z"];
   }
+  MeshData ProcessSingleAsset(const std::filesystem::path& assetJsonAbsPath,
+							  uint32_t                     assetId) {
+	MeshData mD {};
+
+	std::ifstream ss(assetJsonAbsPath);
+	auto          assetJson = json::parse(ss);
+	std::string   assetName = assetJson["name"];
+	auto modelAbsPath = assetJsonAbsPath.parent_path() / assetJson["modelPath"];
+
+	FOO_EDITOR_INFO("Asset {0} loading...", assetName);
+	if (assetJson["format"] == "obj") {
+	  mD.ModelPtr          = AssetLoader::LoadObjModel(modelAbsPath.string());
+	  mD.ModelPtr->AssetId = assetId;
+	  for (int i = 0; i < mD.ModelPtr->m_Meshes.size(); i++) {
+		auto& mesh             = mD.ModelPtr->m_Meshes[i];
+		mesh.materialData.Name = assetJson["meshes"][i]["material"]["name"];
+		mesh.materialData.BaseColorTextureIndex =
+			assetJson["meshes"][i]["material"]["baseColorTextureIndex"];
+		mesh.materialData.NormalTextureIndex =
+			assetJson["meshes"][i]["material"]["normalColorTextureIndex"];
+		mesh.materialData.MetallicRoughnessIndex =
+			assetJson["meshes"][i]["material"]["metallicRoughnessTextureIndex"];
+	  }
+	  mD.ModelPtr->Name = assetJson["name"];
+	  for (const auto& img : assetJson["textures"]) {
+		auto textureAbsPath = assetJsonAbsPath.parent_path() / img["path"];
+		auto texture = AssetLoader::LoadFromFile(textureAbsPath.string());
+		mD.ModelPtr->images.push_back({texture});
+	  }
+	} else if (assetJson["format"] == "glb") {
+	  mD.ModelPtr = AssetLoader::LoadGLTFModel(modelAbsPath.string(), true);
+	  for (int i = 0; i < mD.ModelPtr->m_Meshes.size(); i++) {
+		auto& mesh             = mD.ModelPtr->m_Meshes[i];
+		mesh.materialData.Name = assetJson["meshes"][i]["material"]["name"];
+		mesh.materialData.BaseColorTextureIndex =
+			assetJson["meshes"][i]["material"]["baseColorTextureIndex"];
+		mesh.materialData.NormalTextureIndex =
+			assetJson["meshes"][i]["material"]["normalColorTextureIndex"];
+		mesh.materialData.MetallicRoughnessIndex =
+			assetJson["meshes"][i]["material"]["metallicRoughnessTextureIndex"];
+	  }
+	}
+	ss.close();
+	return mD;
+  }
 
   std::unique_ptr<EditorScene> EditorSceneDeserializer::DeSerialize(
 	  const std::string& scenePath) {
 	using namespace std::filesystem;
-	using json = nlohmann::json;
 	FOO_EDITOR_INFO("Deserializing scene");
 	auto cwd = current_path();
 	cwd.append(scenePath);
@@ -41,56 +86,13 @@ namespace FooGame {
 	scene->Id   = std::move(data["id"]);
 	scene->Name = data["name"];
 	for (auto& asset : data["assets"]) {
-	  MeshData meshData {};
 	  uint32_t assetId = asset["id"];
 
-	  auto assetRelativePath =
-		  sceneBasePath.parent_path() / path(asset["path"]);
+	  auto assetJsonAbsPath = sceneBasePath.parent_path() / path(asset["path"]);
+	  MeshData meshData =
+		  std::move(ProcessSingleAsset(assetJsonAbsPath, assetId));
 
-	  std::ifstream ss(assetRelativePath);
-
-	  auto        assetJson = json::parse(ss);
-	  std::string assetName = assetJson["name"];
-	  FOO_EDITOR_INFO("Asset {0} loading...", assetName);
-	  auto assetAbsPath =
-		  assetRelativePath.parent_path() / assetJson["modelPath"];
-	  if (assetJson["format"] == "obj") {
-		meshData.ModelPtr = AssetLoader::LoadObjModel(assetAbsPath.string());
-		meshData.ModelPtr->AssetId = assetId;
-		for (int i = 0; i < meshData.ModelPtr->m_Meshes.size(); i++) {
-		  auto& mesh             = meshData.ModelPtr->m_Meshes[i];
-		  mesh.materialData.Name = assetJson["meshes"][i]["material"]["name"];
-		  mesh.materialData.BaseColorTextureIndex =
-			  assetJson["meshes"][i]["material"]["baseColorTextureIndex"];
-		  mesh.materialData.NormalTextureIndex =
-			  assetJson["meshes"][i]["material"]["normalColorTextureIndex"];
-		  mesh.materialData.MetallicRoughnessIndex =
-			  assetJson["meshes"][i]["material"]
-					   ["metallicRoughnessTextureIndex"];
-		}
-		meshData.ModelPtr->Name = assetJson["name"];
-		for (const auto& img : assetJson["textures"]) {
-		  auto textureAbsPath = assetRelativePath.parent_path() / img["path"];
-		  auto texture = AssetLoader::LoadFromFile(textureAbsPath.string());
-		  meshData.ModelPtr->images.push_back({texture});
-		}
-	  } else if (assetJson["format"] == "glb") {
-		meshData.ModelPtr =
-			AssetLoader::LoadGLTFModel(assetAbsPath.string(), true);
-		for (int i = 0; i < meshData.ModelPtr->m_Meshes.size(); i++) {
-		  auto& mesh             = meshData.ModelPtr->m_Meshes[i];
-		  mesh.materialData.Name = assetJson["meshes"][i]["material"]["name"];
-		  mesh.materialData.BaseColorTextureIndex =
-			  assetJson["meshes"][i]["material"]["baseColorTextureIndex"];
-		  mesh.materialData.NormalTextureIndex =
-			  assetJson["meshes"][i]["material"]["normalColorTextureIndex"];
-		  mesh.materialData.MetallicRoughnessIndex =
-			  assetJson["meshes"][i]["material"]
-					   ["metallicRoughnessTextureIndex"];
-		}
-	  }
-	  scene->MeshDatas.push_back(meshData);
-	  ss.close();
+	  scene->MeshDatas.push_back(std::move(meshData));
 	}
 	FOO_EDITOR_INFO("Scene data loaded successfully");
 	stream.close();
