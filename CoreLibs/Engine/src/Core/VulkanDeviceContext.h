e3de1b4d665bc3c9256ed23e358aa249532fbf43
Rüçhan Adıgüzel <44341663+ruchan-o7@users.noreply.github.com>
2024-06-11 18:16:31 +0300

    device context implementing

--- CoreLibs/Engine/src/Core/VulkanDeviceContext.h
+++ CoreLibs/Engine/src/Core/VulkanDeviceContext.h
@@ -1,9 +1,15 @@
 #pragma once
 #include <cassert>
 #include "Types.h"
+#include "vulkan/vulkan_core.h"
+namespace vke
+{
+    class DescriptorAllocatorPool;
+}
 namespace ENGINE_NAMESPACE
 {
     class RenderDevice;
+    class VulkanBuffer;
     class VulkanDeviceContext
     {
         public:
@@ -25,8 +31,20 @@ namespace ENGINE_NAMESPACE
             RenderDevice* GetVkDevice() const { return m_pRenderDevice; }
 
         private:
+        private:
             RenderDevice* m_pRenderDevice;
             DeviceContextDesc m_DevDesc;
             EngineCreateInfo m_EngineCI;
+            std::unique_ptr<vke::DescriptorAllocatorPool> m_DescriptorAllocator;
+
+            std::vector<VkSemaphore> m_WaitSemaphores;  // image available
+            std::vector<VkSemaphore> m_RenderFinishedSemaphores;
+            VkCommandPool m_CmdPool         = nullptr;
+            VkFramebuffer m_FrameBuffer     = nullptr;
+            VkRenderPass m_VkRenderPass     = nullptr;
+            VkCommandBuffer m_CommandBuffer = nullptr;
+
+            std::vector<std::unique_ptr<VulkanBuffer>> m_UniformBuffers;
+            VulkanBuffer* m_CurrentUniformBuffer;
     };
 }  // namespace ENGINE_NAMESPACE
