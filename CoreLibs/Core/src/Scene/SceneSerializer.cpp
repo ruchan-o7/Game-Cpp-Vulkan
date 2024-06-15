1ae6f7cb7f98d35d9b3c82d7ebc7c06d49fe37d5
Rüçhan Adıgüzel <44341663+ruchan-o7@users.noreply.github.com>
2024-06-15 13:00:00 +0300

    scene serializer refactoring

--- CoreLibs/Core/src/Scene/SceneSerializer.cpp
+++ CoreLibs/Core/src/Scene/SceneSerializer.cpp
@@ -21,6 +21,122 @@ namespace FooGame
     SceneSerializer::SceneSerializer(Scene* scene) : m_pScene(scene)
     {
     }
+    static void DeSerializeEntity(Entity& entity, json& sceneEntitiesNode)
+    {
+        if (!entity.HasComponent<IDComponent>())
+        {
+            uint64_t id             = entity.GetComponent<IDComponent>().ID;
+            sceneEntitiesNode["id"] = id;
+        }
+        if (!entity.HasComponent<TagComponent>())
+        {
+            auto tag                          = entity.GetComponent<TagComponent>().Tag;
+            sceneEntitiesNode["tagComponent"] = {"tag", tag};
+        }
+
+        if (!entity.HasComponent<TransformComponent>())
+        {
+            auto transform = entity.GetComponent<TransformComponent>();
+
+            sceneEntitiesNode["transformComponent"] = json::object({
+                {"translation",
+                 {transform.Translation.x, transform.Translation.y, transform.Translation.z}      },
+                {   "rotation", {transform.Rotation.x, transform.Rotation.y, transform.Rotation.z}},
+                {      "scale",          {transform.Scale.x, transform.Scale.y, transform.Scale.z}}
+            });
+        }
+        if (!entity.HasComponent<MeshRendererComponent>())
+        {
+            auto mesh                          = entity.GetComponent<MeshRendererComponent>();
+            sceneEntitiesNode["meshComponent"] = json::object({
+                {"modelPath",    mesh.ModelPath},
+                { "material", mesh.MaterialName}
+            });
+        }
+        if (!entity.HasComponent<ScriptComponent>())
+        {
+            auto scripts                         = entity.GetComponent<ScriptComponent>();
+            sceneEntitiesNode["scriptComponent"] = json::array();
+            for (auto [name, script] : scripts.Scripts)
+            {
+                sceneEntitiesNode["scriptComponent"].push_back(name);
+            }
+        }
+    }
+    static void SerializeEntity(const json& entityJson, Scene* scene,
+                                const std::filesystem::path& assetPath)
+    {
+        uint64_t uuid = entityJson["id"];
+        std::string name;
+
+        auto tagComponent = entityJson["tagComponent"];
+        if (!tagComponent.empty())
+        {
+            name = tagComponent["tag"];
+        }
+        FOO_ENGINE_TRACE("Deserialized entity with ID = {0}, name = {1}", uuid, name);
+        Entity deserializedEntity = scene->CreateEntityWithUUID(uuid, name);
+
+        auto transformComponent = entityJson["transformComponent"];
+        assert(!transformComponent.empty());
+        if (!transformComponent.empty())
+        {
+            auto& tc    = deserializedEntity.GetComponent<TransformComponent>();
+            auto toVec3 = [](auto comp) { return glm::vec3{comp[0], comp[1], comp[2]}; };
+
+            tc.Translation = toVec3(transformComponent["translation"]);
+            tc.Scale       = toVec3(transformComponent["scale"]);
+            tc.Rotation    = toVec3(transformComponent["rotation"]);
+        }
+        auto meshComponent = entityJson["meshComponent"];
+        if (!meshComponent.empty())
+        {
+            auto& mc        = deserializedEntity.AddComponent<MeshRendererComponent>();
+            auto modelPath  = std::filesystem::path(meshComponent["modelPath"]);
+            mc.ModelPath    = modelPath.string();
+            mc.MaterialName = meshComponent["material"];
+
+            auto modelExtension = modelPath.extension().string();
+            if (modelExtension == ".obj")
+            {
+                auto p = (assetPath / modelPath);
+                AssetManager::LoadObjModel(p.string(), p.filename().string());
+                mc.ModelName = p.filename().string();
+            }
+            else if (modelExtension == ".glb")
+            {
+                auto p = (assetPath / modelPath);
+                AssetManager::LoadGLTFModel(p.string(), p.filename().string(), true);
+                mc.ModelName = p.filename().string();
+            }
+            else if (modelExtension == ".gltf")
+            {
+                auto p = (assetPath / modelPath);
+                AssetManager::LoadGLTFModel(p.string(), p.filename().string(), false);
+                mc.ModelName = p.filename().string();
+            }
+        }
+        auto scriptComponent = entityJson["scriptComponent"];
+        if (!scriptComponent.empty())
+        {
+            auto& sc = deserializedEntity.AddComponent<ScriptComponent>();
+            for (auto& sname : scriptComponent)
+            {
+                if (sname == "Rotate")
+                {
+                    sc.Bind<RotateScript>("RotateScript");
+                }
+                else if (sname == "ScaleYoink")
+                {
+                    sc.Bind<ScaleYoink>("ScaleYoink");
+                }
+                else
+                {
+                    FOO_ENGINE_ERROR("Script with name: {0} could not found");
+                }
+            }
+        }
+    }
     void SceneSerializer::DeSerialize(const std::string& path)
     {
         json scene;
@@ -44,46 +160,7 @@ namespace FooGame
                 continue;
             }
             json entityJson = json::object();
-            if (!ent.HasComponent<IDComponent>())
-            {
-                uint64_t id      = ent.GetComponent<IDComponent>().ID;
-                entityJson["id"] = id;
-            }
-            if (!ent.HasComponent<TagComponent>())
-            {
-                auto tag                   = ent.GetComponent<TagComponent>().Tag;
-                entityJson["tagComponent"] = {"tag", tag};
-            }
-
-            if (!ent.HasComponent<TransformComponent>())
-            {
-                auto transform = ent.GetComponent<TransformComponent>();
-
-                entityJson["transformComponent"] = json::object({
-                    {"translation",
-                     {transform.Translation.x, transform.Translation.y, transform.Translation.z}},
-                    {   "rotation",
-                     {transform.Rotation.x, transform.Rotation.y, transform.Rotation.z}         },
-                    {      "scale",    {transform.Scale.x, transform.Scale.y, transform.Scale.z}}
-                });
-            }
-            if (!ent.HasComponent<MeshRendererComponent>())
-            {
-                auto mesh                   = ent.GetComponent<MeshRendererComponent>();
-                entityJson["meshComponent"] = json::object({
-                    {"modelPath",    mesh.ModelPath},
-                    { "material", mesh.MaterialName}
-                });
-            }
-            if (!ent.HasComponent<ScriptComponent>())
-            {
-                auto scripts                  = ent.GetComponent<ScriptComponent>();
-                entityJson["scriptComponent"] = json::array();
-                for (auto [name, script] : scripts.Scripts)
-                {
-                    entityJson["scriptComponent"].push_back(name);
-                }
-            }
+            DeSerializeEntity(ent, entityJson);
             scene["entities"].push_back(entityJson);
         }
 
@@ -139,76 +216,7 @@ namespace FooGame
         {
             for (auto entity : entities)
             {
-                uint64_t uuid = entity["id"];
-                std::string name;
-
-                auto tagComponent = entity["tagComponent"];
-                if (!tagComponent.empty())
-                {
-                    name = tagComponent["tag"];
-                }
-                FOO_ENGINE_TRACE("Deserialized entity with ID = {0}, name = {1}", uuid, name);
-                Entity deserializedEntity = m_pScene->CreateEntityWithUUID(uuid, name);
-
-                auto transformComponent = entity["transformComponent"];
-                assert(!transformComponent.empty());
-                if (!transformComponent.empty())
-                {
-                    auto& tc    = deserializedEntity.GetComponent<TransformComponent>();
-                    auto toVec3 = [](auto comp) { return glm::vec3{comp[0], comp[1], comp[2]}; };
-
-                    tc.Translation = toVec3(transformComponent["translation"]);
-                    tc.Scale       = toVec3(transformComponent["scale"]);
-                    tc.Rotation    = toVec3(transformComponent["rotation"]);
-                }
-                auto meshComponent = entity["meshComponent"];
-                if (!meshComponent.empty())
-                {
-                    auto& mc        = deserializedEntity.AddComponent<MeshRendererComponent>();
-                    auto modelPath  = std::filesystem::path(meshComponent["modelPath"]);
-                    mc.ModelPath    = modelPath.string();
-                    mc.MaterialName = meshComponent["material"];
-
-                    auto modelExtension = modelPath.extension().string();
-                    if (modelExtension == ".obj")
-                    {
-                        auto p = (assetPath / modelPath);
-                        AssetManager::LoadObjModel(p.string(), p.filename().string());
-                        mc.ModelName = p.filename().string();
-                    }
-                    else if (modelExtension == ".glb")
-                    {
-                        auto p = (assetPath / modelPath);
-                        AssetManager::LoadGLTFModel(p.string(), p.filename().string(), true);
-                        mc.ModelName = p.filename().string();
-                    }
-                    else if (modelExtension == ".gltf")
-                    {
-                        auto p = (assetPath / modelPath);
-                        AssetManager::LoadGLTFModel(p.string(), p.filename().string(), false);
-                        mc.ModelName = p.filename().string();
-                    }
-                }
-                auto scriptComponent = entity["scriptComponent"];
-                if (!scriptComponent.empty())
-                {
-                    auto& sc = deserializedEntity.AddComponent<ScriptComponent>();
-                    for (auto& sname : scriptComponent)
-                    {
-                        if (sname == "Rotate")
-                        {
-                            sc.Bind<RotateScript>("RotateScript");
-                        }
-                        else if (sname == "ScaleYoink")
-                        {
-                            sc.Bind<ScaleYoink>("ScaleYoink");
-                        }
-                        else
-                        {
-                            FOO_ENGINE_ERROR("Script with name: {0} could not found");
-                        }
-                    }
-                }
+                SerializeEntity(entity, m_pScene, assetPath);
             }
         }
     }
