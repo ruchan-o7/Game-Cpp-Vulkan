0cfdf797c3cd9e8e274c86c3c50049268227c4ee
Rüçhan Adıgüzel <44341663+ruchan-o7@users.noreply.github.com>
2025-05-28 21:47:33 +0300

    wipe all engine code

--- Core/src/Engine/Geometry/Model.cpp
+++
@@ -1,8 +0,0 @@
-#include "Model.h"
-namespace FooGame
-{
-    // Model::Model(std::vector<Mesh>&& meshes) : Meshes(std::move(meshes))
-    // {
-    // }
-
-}  // namespace FooGame
--- Core/src/Engine/Implementations/tiny_gltf.cpp
+++
@@ -1,3 +0,0 @@
-#define TINYGLTF_IMPLEMENTATION
-#define TINYGLTF_NOEXCEPTION
-#include <tiny_gltf.h>
