851baf1193cb3f9697da30fe65857e03cc72841b
Rüçhan Adıgüzel <44341663+ruchan-o7@users.noreply.github.com>
2025-05-30 17:34:00 +0300

    add basic triangle for testing

--- Assets/Shaders/compile.bat
+++ Assets/Shaders/compile.bat
@@ -1,4 +1,6 @@
 glslc -g shader.vert -o vert.spv
+glslc -g shader.frag -o frag.spv
 glslc -g QuadShader.vert -o QuadShaderVert.spv
 glslc -g QuadShader.frag -o QuadShaderFrag.spv
-glslc -g shader.frag -o frag.spv
+glslc -g triangle.vert -o triangle_vert.spv
+glslc -g triangle.frag -o triangle_frag.spv
