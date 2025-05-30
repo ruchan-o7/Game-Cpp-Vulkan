f1957596347630cb15a26a972f8f2bff52286381
Rüçhan Adıgüzel <44341663+ruchan-o7@users.noreply.github.com>
2025-05-30 16:32:02 +0300

    add create func for vulkan objs

--- Core/src/Renderer/VulkanLogicalDevice.cpp
+++ Core/src/Renderer/VulkanLogicalDevice.cpp
@@ -1,12 +1,19 @@
-#pragma once
 #include "VulkanLogicalDevice.h"
 #include "vulkan/vulkan_core.h"
+#include "VulkanObject.h"
+
 namespace fg {
 
-VkShaderModule VulkanLogicalDevice::CreateShader(const VkShaderModuleCreateInfo& info,
-                                                 const char* name) {
-  VkShaderModule module = VK_NULL_HANDLE;
-  auto res = vkCreateShaderModule(GetHandle(), &info, GetAllocator(), &module);
-  return module;
+ShaderModuleWrapper VulkanLogicalDevice::CreateShader(const VkShaderModuleCreateInfo& info,
+                                                      const char* name) {
+  VkShaderModule module;
+  auto res = vkCreateShaderModule(m_Device, &info, m_Allocator, &module);
+  return ShaderModuleWrapper(std::move(module), GetPtr());
 }
+
+void VulkanLogicalDevice::DestroyObject(ShaderModuleWrapper&& module) const {
+  vkDestroyShaderModule(m_Device, module, m_Allocator);
+  module.m_VulkanObject = VK_NULL_HANDLE;
+}
+
 }  // namespace fg
--- Core/src/Renderer/VulkanLogicalDevice.h
+++ Core/src/Renderer/VulkanLogicalDevice.h
@@ -1,13 +1,34 @@
 #pragma once
 
+#include <memory>
 #include "vulkan/vulkan_core.h"
+
 namespace fg {
-class VulkanLogicalDevice {
+
+template <class T>
+class VulkanObject;
+
+using ShaderModuleWrapper = VulkanObject<VkShaderModule>;
+
+class VulkanLogicalDevice : public std::enable_shared_from_this<VulkanLogicalDevice> {
   public:
     VulkanLogicalDevice(VkDevice device, uint32_t queueIndex,
                         const VkAllocationCallbacks* allocator)
         : m_Device(device), m_Allocator(allocator) {
       vkGetDeviceQueue(m_Device, queueIndex, 0, &m_Queue);
+    }
+
+    VulkanLogicalDevice(const VulkanLogicalDevice&) = delete;
+    VulkanLogicalDevice(VulkanLogicalDevice&&) = delete;
+    VulkanLogicalDevice& operator=(const VulkanLogicalDevice&) = delete;
+    VulkanLogicalDevice& operator=(VulkanLogicalDevice&&) = delete;
+
+    std::shared_ptr<VulkanLogicalDevice> GetPtr() {
+      return shared_from_this();
+    }
+
+    std::shared_ptr<const VulkanLogicalDevice> GetPtr() const {
+      return shared_from_this();
     }
 
     VkDevice GetHandle() const {
@@ -16,8 +37,10 @@ class VulkanLogicalDevice {
     const VkAllocationCallbacks* GetAllocator() const {
       return m_Allocator;
     }
+    void DestroyObject(ShaderModuleWrapper&& module) const;
 
-    VkShaderModule CreateShader(const VkShaderModuleCreateInfo& info, const char* name = nullptr);
+    ShaderModuleWrapper CreateShader(const VkShaderModuleCreateInfo& info,
+                                     const char* name = nullptr);
 
   private:
     const VkAllocationCallbacks* m_Allocator;
