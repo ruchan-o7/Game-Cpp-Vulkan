f74286d22a0fe29164dae548d5c0425fc636824a
Rüçhan Adıgüzel <44341663+ruchan-o7@users.noreply.github.com>
2025-05-30 16:00:35 +0300

    add allocator inside device

--- Core/src/Renderer/VulkanLogicalDevice.cpp
+++ Core/src/Renderer/VulkanLogicalDevice.cpp
@@ -1,11 +1,4 @@
 #pragma once
 #include "VulkanLogicalDevice.h"
 #include "vulkan/vulkan_core.h"
-namespace fg {
-
-VulkanLogicalDevice::VulkanLogicalDevice(VkDevice device, uint32_t queueIndex)
-    : m_Device(device), m_QueueIndex(queueIndex) {
-  vkGetDeviceQueue(m_Device, queueIndex, 0, &m_Queue);
-}
-
-}  // namespace fg
+namespace fg { }  // namespace fg
--- Core/src/Renderer/VulkanLogicalDevice.h
+++ Core/src/Renderer/VulkanLogicalDevice.h
@@ -4,13 +4,21 @@
 namespace fg {
 class VulkanLogicalDevice {
   public:
-    VulkanLogicalDevice(VkDevice device, uint32_t queueIndex);
+    VulkanLogicalDevice(VkDevice device, uint32_t queueIndex,
+                        const VkAllocationCallbacks* allocator)
+        : m_Device(device), m_Allocator(allocator) {
+      vkGetDeviceQueue(m_Device, queueIndex, 0, &m_Queue);
+    }
 
     VkDevice GetHandle() const {
       return m_Device;
     }
+    const VkAllocationCallbacks* GetAllocator() const {
+      return m_Allocator;
+    }
 
   private:
+    const VkAllocationCallbacks* m_Allocator;
     VkDevice m_Device;
     VkQueue m_Queue;
     uint32_t m_QueueIndex;
