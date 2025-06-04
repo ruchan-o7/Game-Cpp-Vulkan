e2626e19dea2de6488d161833fac66ce51aba2b8
Rüçhan Adıgüzel <44341663+ruchan-o7@users.noreply.github.com>
2025-06-04 18:59:08 +0300

    omit 1 for Count var

--- Core/src/Core/Application.cpp
+++ Core/src/Core/Application.cpp
@@ -123,8 +123,8 @@ Application::Application(const ApplicationSpecifications& spec) : m_Specs(spec) 
         {0, sizeof(glm::vec2) + sizeof(glm::vec3)}
     };
     pipeDesc.ShaderVariables = {
-        {0,         VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1,   VK_SHADER_STAGE_VERTEX_BIT},
-        {1, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1, VK_SHADER_STAGE_FRAGMENT_BIT},
+        {0, 0,         VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,   VK_SHADER_STAGE_VERTEX_BIT},
+        {1, 0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, VK_SHADER_STAGE_FRAGMENT_BIT},
     };
 
     m_Pipeline = m_Renderer->CreateGraphicsPipeline(pipeDesc);
