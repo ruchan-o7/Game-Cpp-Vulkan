538d3b2ac8c856a28023f4bb07d4ba7b2b10e82c
Rüçhan Adıgüzel <44341663+ruchan-o7@users.noreply.github.com>
2025-06-11 00:19:59 +0300

    fix intrusive ref counting

--- Core/src/Core/Ref.h
+++ Core/src/Core/Ref.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <atomic>
+#include <type_traits>
+#include "Assert.h"
 
 namespace fg {
 
@@ -9,24 +11,27 @@ class WeakRef;
 template <class T>
 class Ref;
 
-template <class T>
+class RefBase;
+
 class ReferenceCounter {
   public:
-    template <class... Args>
-    explicit ReferenceCounter(Args&&... args) {
+    uint32_t AddStrong() const {
+      return m_StrongRef.fetch_add(1, std::memory_order_relaxed);
     }
-
-    void AddStrong() const {
-      m_StrongRef.fetch_add(1, std::memory_order_relaxed);
+    uint32_t RelRef() const {
+      return m_StrongRef.fetch_sub(1, std::memory_order_acq_rel);
     }
-    void RelRef() const {
-      auto count = m_StrongRef.fetch_sub(1, std::memory_order_acq_rel);
+    uint32_t AddWeakRef() const {
+      return m_WeakRef.fetch_add(1, std::memory_order_relaxed);
     }
-    void AddWeakRef() const {
-      m_WeakRef.fetch_add(1, std::memory_order_relaxed);
+    uint32_t RelWeakRef() const {
+      return m_WeakRef.fetch_sub(1, std::memory_order_acq_rel);
     }
-    void RelWeakRef() const {
-      m_WeakRef.fetch_sub(1, std::memory_order_acq_rel);
+    uint32_t GetStrongReferenceCount() const {
+      return m_StrongRef;
+    }
+    uint32_t GetWeakReferenceCount() const {
+      return m_WeakRef;
     }
 
   private:
@@ -36,51 +41,59 @@ class ReferenceCounter {
 
 class RefBase {
   public:
+    RefBase(ReferenceCounter* counter) : m_RefCounter(counter) {
+    }
     virtual ~RefBase() = default;
 
+    ReferenceCounter* GetRefCounter() const {
+      return m_RefCounter;
+    }
     void AddRef() const {
-      m_StrongRef.fetch_add(1, std::memory_order_relaxed);
+      m_RefCounter->AddStrong();
     }
     void RelRef() const {
-      auto count = m_StrongRef.fetch_sub(1, std::memory_order_acq_rel);
-      if (count == 1) {
-        delete this;
-      }
+      m_RefCounter->RelRef();
     }
     void AddWeakRef() const {
-      m_WeakRef.fetch_add(1, std::memory_order_relaxed);
+      m_RefCounter->AddWeakRef();
     }
     void RelWeakRef() const {
-      m_WeakRef.fetch_sub(1, std::memory_order_acq_rel);
+      m_RefCounter->RelWeakRef();
     }
     uint32_t RefCount() const {
-      return m_StrongRef;
+      return m_RefCounter->GetStrongReferenceCount();
     }
     uint32_t WeakRefCount() const {
-      return m_WeakRef;
+      return m_RefCounter->GetWeakReferenceCount();
     }
     void Release() {
       delete this;
     }
 
   private:
-    mutable std::atomic<uint32_t> m_StrongRef = 0;
-    mutable std::atomic<uint32_t> m_WeakRef = 0;
+    ReferenceCounter* m_RefCounter;
 };
 
 template <class T>
 class Ref {
   public:
     Ref(T* ptr = nullptr) : m_Ptr(ptr) {
-      AddStrong();
+      if (m_Ptr) {
+        m_RefCounter = m_Ptr->GetRefCounter();
+        AddStrong();
+      }
     }
 
     Ref(const Ref& other) : m_Ptr(other.m_Ptr) {
-      AddStrong();
+      if (other) {
+        m_RefCounter = other.m_RefCounter;
+        AddStrong();
+      }
     }
 
-    Ref(Ref&& other) noexcept : m_Ptr(other.m_Ptr) {
+    Ref(Ref&& other) noexcept : m_Ptr(other.m_Ptr), m_RefCounter(other.m_RefCounter) {
       other.m_Ptr = nullptr;
+      other.m_RefCounter = nullptr;
     }
 
     ~Ref() {
@@ -91,6 +104,9 @@ class Ref {
       if (m_Ptr != ptr) {
         ReleaseStrong();
         m_Ptr = ptr;
+        if (ptr) {
+          m_RefCounter = ptr->GetRefCounter();
+        }
         AddStrong();
       }
       return *this;
@@ -100,6 +116,7 @@ class Ref {
       if (this != &other) {
         ReleaseStrong();
         m_Ptr = other.m_Ptr;
+        m_RefCounter = other.m_RefCounter;
         AddStrong();
       }
       return *this;
@@ -107,9 +124,11 @@ class Ref {
 
     Ref& operator=(Ref&& other) noexcept {
       if (this != &other) {
-        ReleaseStrong();
+        // ReleaseStrong();
         m_Ptr = other.m_Ptr;
+        m_RefCounter = other.m_RefCounter;
         other.m_Ptr = nullptr;
+        other.m_RefCounter = nullptr;
       }
       return *this;
     }
@@ -135,15 +154,17 @@ class Ref {
     }
 
     explicit operator bool() const {
-      return m_Ptr != nullptr;
+      return m_RefCounter != nullptr &&
+             m_RefCounter->GetStrongReferenceCount() != 0;  //|| m_Ptr != nullptr;
     }
 
   private:
     T* m_Ptr = nullptr;
+    ReferenceCounter* m_RefCounter = nullptr;
 
     void AddStrong() {
-      if (m_Ptr) {
-        m_Ptr->AddRef();
+      if (m_RefCounter) {
+        m_RefCounter->AddStrong();
       }
     }
 
@@ -151,17 +172,25 @@ class Ref {
       if (!m_Ptr) {
         return;
       }
-
-      m_Ptr->RelRef();
+      FOO_ASSERT(m_RefCounter != nullptr, "Some thing happened here");
+      uint32_t count = m_RefCounter->RelRef();
+      if (count == 1) {
+        delete m_Ptr;
+      }
+      if (m_RefCounter->GetWeakReferenceCount() == 0 &&
+          m_RefCounter->GetStrongReferenceCount() == 0) {
+        delete m_RefCounter;
+      }
+      m_RefCounter = nullptr;
       m_Ptr = nullptr;
     }
     void AddWeakRef() {
-      if (m_Ptr) {
+      if (m_RefCounter) {
         m_Ptr->AddWeakRef();
       }
     }
     void RelWeakRef() {
-      if (m_Ptr) {
+      if (m_RefCounter) {
         m_Ptr->RelWeakRef();
       }
     }
@@ -171,29 +200,27 @@ class Ref {
 template <typename T>
 class WeakRef {
   public:
-    WeakRef() = default;
+    WeakRef() : m_Ptr(nullptr), m_RefCounter(nullptr) {
+    }
 
     WeakRef(const Ref<T>& strongRef) {
       if (strongRef) {
-        strongRef->AddWeakRef();
+        m_RefCounter = strongRef.m_RefCounter;
+        m_RefCounter->AddWeakRef();
       }
       m_Ptr = strongRef.m_Ptr;
-    }
-    WeakRef(Ref<T> ref) {
-      if (ref) {
-        ref->AddWeakRef();
-      }
-      m_Ptr = ref.m_Ptr;
     }
 
     WeakRef(const WeakRef& other) : m_Ptr(other.m_Ptr) {
       if (m_Ptr) {
-        m_Ptr->AddWeakRef();
+        m_RefCounter = other.m_RefCounter;
+        m_RefCounter->AddWeakRef();
       }
     }
 
-    WeakRef(WeakRef&& other) noexcept : m_Ptr(other.m_Ptr) {
+    WeakRef(WeakRef&& other) noexcept : m_Ptr(other.m_Ptr), m_RefCounter(other.m_RefCounter) {
       other.m_Ptr = nullptr;
+      other.m_RefCounter = nullptr;
     }
 
     ~WeakRef() {
@@ -204,8 +231,9 @@ class WeakRef {
       if (this != &other) {
         ReleaseWeak();
         m_Ptr = other.m_Ptr;
-        if (m_Ptr) {
-          m_Ptr->AddWeakRef();
+        m_RefCounter = other->m_RefCounter;
+        if (m_RefCounter) {
+          m_RefCounter->AddWeakRef();
         }
       }
       return *this;
@@ -214,33 +242,49 @@ class WeakRef {
     WeakRef& operator=(WeakRef&& other) noexcept {
       if (this != &other) {
         m_Ptr = other.m_Ptr;
+        m_RefCounter = other.m_RefCounter;
         other.m_Ptr = nullptr;
+        other.m_RefCounter = nullptr;
       }
       return *this;
     }
 
     Ref<T> Lock() const {
+      if (!m_RefCounter) {
+        return nullptr;
+      }
+      if (m_RefCounter->GetStrongReferenceCount() == 0) {
+        return nullptr;
+      }
       return Ref<T>(m_Ptr);
     }
 
     bool Expired() const {
-      return m_Ptr && m_Ptr->RefCount() > 0;
+      if (!m_RefCounter) {
+        return true;
+      }
+      return m_RefCounter->GetStrongReferenceCount() > 0;
     }
 
   private:
     T* m_Ptr = nullptr;
-
+    ReferenceCounter* m_RefCounter = nullptr;
     void ReleaseWeak() {
-      if (m_Ptr) {
-        m_Ptr->RelWeakRef();
+      if (m_RefCounter) {
+        m_RefCounter->RelWeakRef();
+        if (m_RefCounter->GetWeakReferenceCount() == 0 &&
+            m_RefCounter->GetStrongReferenceCount() == 0) {
+          delete m_RefCounter;
+        }
       }
       m_Ptr = nullptr;
+      m_RefCounter = nullptr;
     }
 };
 
 template <class T, class... Args>
 Ref<T> MakeRef(Args&&... args) {
-  return Ref<T>(new T(std::forward<Args>(args)...));
+  return Ref<T>(new T(new ReferenceCounter {}, std::forward<Args>(args)...));
 }
 
 }  // namespace fg
